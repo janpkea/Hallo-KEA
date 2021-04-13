a7f6d77e544af6fe3163b0a348dbc1029a00f102
Lars <larsriel@gmail.com>
2021-04-13 13:57:49 +0200

    Made an Awesome class

---
+++ src/Awesome.java
@@ -0,0 +1,12 @@
+public class Awesome {
+
+    private String msg;
+
+    public Awesome() {
+        msg = "Hello Universe";
+    }
+
+    public void printMsg() {
+        System.out.println(msg);
+    }
+}
